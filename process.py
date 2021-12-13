29fc9886e25d3a0fa4f4f445ae8ff7b8d0cee8fa
marija.petrovic <marijap@protonmail.com>
2021-12-13 01:01:46 +0100

    vectorized landmarks

--- process.py
+++ process.py
@@ -1,3 +1,4 @@
+import math
 import os
 import numpy as np
 import cv2
@@ -19,9 +20,9 @@ def train_or_load_facial_expression_recognition_model(train_image_paths, train_i
     """
     Procedura prima listu putanja do fotografija za obucavanje i listu labela za svaku fotografiju iz prethodne liste
 
-    Kada se procedura pozove, ona treba da trenira model ako on nije istraniran, ili da ga samo ucita ako je prethodno istreniran. 
+    Kada se procedura pozove, ona treba da trenira model ako on nije istraniran, ili da ga samo ucita ako je prethodno istreniran.
     Ako serijalizujete model, serijalizujte ga odmah pored main.py, bez kreiranja dodatnih foldera.
-    Napomena: Platforma ne vrsi download serijalizovanih modela i bilo kakvih foldera i sve ce se na njoj ponovo trenirati (vodite racuna o vremenu). 
+    Napomena: Platforma ne vrsi download serijalizovanih modela i bilo kakvih foldera i sve ce se na njoj ponovo trenirati (vodite racuna o vremenu).
     Serijalizaciju mozete raditi samo da ubrzate razvoj lokalno, da se model ne trenira svaki put.
 
     Vreme izvrsavanja celog resenja je ograniceno na maksimalno 1h.
@@ -32,27 +33,69 @@ def train_or_load_facial_expression_recognition_model(train_image_paths, train_i
     """
     # TODO - Istrenirati model ako vec nije istreniran
 
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+
     images = []
     for path in train_image_paths:
-        img = resize_image(load_image(path))
-        images.append(img)
+        image = resize_image(load_image(path))
+        images.append(image)
 
     print(len(images))
 
     features = []
     labels = []
-
-    hog = get_hog(img)
+    #hog = get_hog(image)
 
     for i in range(len(images) - len(images) + 1):
         for j in range(len(train_image_labels)):
-            features.append(hog.compute(images[j]))
-            labels.append(train_image_labels[j])
+            detections = detector(images[j], 1)
+            for k, d in enumerate(detections):
+                shape = predictor(images[j], d)
+                xlist = []
+                ylist = []
+                for i in range(1, 68):
+                    xlist.append(float(shape.part(i).x))
+                    ylist.append(float(shape.part(i).y))
+
+                xmean = np.mean(xlist)
+                ymean = np.mean(ylist)
+                xcentral = [(x - xmean) for x in xlist]
+                ycentral = [(y - ymean) for y in ylist]
+
+                if xlist[26] == xlist[29]:
+                    anglenose = 0
+                else:
+                    anglenose = int(math.atan((ylist[26] - ylist[29]) / (xlist[26] - xlist[29])))
+
+                if anglenose < 0:
+                    anglenose += 90
+                else:
+                    anglenose -= 90
+
+                landmarks_vectorized = []
+                for x, y, w, z in zip(xcentral, ycentral, xlist, ylist):
+                    landmarks_vectorized.append(x)
+                    landmarks_vectorized.append(y)
+                    meannp = np.asarray((ymean, xmean))
+                    coornp = np.asarray((z, w))
+                    dist = np.linalg.norm(coornp - meannp)
+                    angle_relative = (math.atan((z - ymean) / (w - xmean)) * 180 / math.pi) - anglenose
+                    landmarks_vectorized.append(dist)
+                    landmarks_vectorized.append(angle_relative)
+
+            if len(detections) < 1:
+                pass
+            else:
+                features.append(landmarks_vectorized)
+                labels.append(train_image_labels[j])
 
     features = np.array(features, dtype="object")
+    print(features.shape)
     y = np.array(labels, dtype="object")
 
-    x_train = reshape_data(features)
+    x_train = features
+    #x_train = reshape_data(features)
     print(x_train.shape)
     print('Train shape: ', x_train.shape, y.shape)
 
@@ -80,7 +123,7 @@ def extract_facial_expression_from_image(trained_model, image_path):
     # TODO - Prepoznati ekspresiju lica i vratiti njen naziv (kao string, iz skupa mogucih vrednosti)
 
     facial_expression = ""
-    # possible_facial_expressions = ["anger", "contempt", "disgust", "happiness", "neutral", "sadness", "surprise"]
+    possible_facial_expressions = ["anger", "contempt", "disgust", "happiness", "neutral", "sadness", "surprise"]
 
     detector = dlib.get_frontal_face_detector()
     predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
@@ -89,24 +132,65 @@ def extract_facial_expression_from_image(trained_model, image_path):
     image = cv2.resize(image, (190, 262))
     rects = detector(image, 1)
 
-    shape = []
-    for (i, rect) in enumerate(rects):
-        shape = predictor(image, rect)
-        shape = face_utils.shape_to_np(shape)
-        print("Dimenzije prediktor matrice: {0}".format(shape.shape))
-        print("Prva 3 elementa matrice")
-        print(shape[:3])
+    # shape = []
+    # for (i, rect) in enumerate(rects):
+    #     shape = predictor(image, rect)
+    #     shape = face_utils.shape_to_np(shape)
+    #     print("Dimenzije prediktor matrice: {0}".format(shape.shape))
+    #     print("Prva 3 elementa matrice")
+    #     print(shape[:3])
+    #
+    feature = []
+    detections = detector(image, 1)
+    for k,d in enumerate(detections):
+        shape = predictor(image, d)
+        xlist = []
+        ylist = []
+        for i in range(1, 68):
+            xlist.append(float(shape.part(i).x))
+            ylist.append(float(shape.part(i).y))
 
-    #display_image(image)
-    hog = get_hog(image)
+        xmean = np.mean(xlist)
+        ymean = np.mean(ylist)
+        xcentral = [(x - xmean) for x in xlist]
+        ycentral = [(y - ymean) for y in ylist]
 
-    feature = hog.compute(image)
-    feature = reshape_data_extract(feature)
+        if xlist[26] == xlist[29]:
+            anglenose = 0
+        else:
+            anglenose = int(math.atan((ylist[26] - ylist[29]) / (xlist[26] - xlist[29])))
 
-    # if len(shape) == 0:
-    #     facial_expression = 'anger'
-    # else:
-    facial_expression = trained_model.predict(feature)[0]
+        if anglenose < 0 :
+            anglenose += 90
+        else:
+            anglenose -= 90
+
+        landmarks_vectorized = []
+        for x, y, w, z in zip(xcentral, ycentral, xlist, ylist):
+            landmarks_vectorized.append(x)
+            landmarks_vectorized.append(y)
+            meannp = np.asarray((ymean, xmean))
+            coornp = np.asarray((z, w))
+            dist = np.linalg.norm(coornp - meannp)
+            angle_relative = (math.atan((z - ymean) / (w - xmean)) * 180 / math.pi) - anglenose
+            landmarks_vectorized.append(dist)
+            landmarks_vectorized.append(angle_relative)
+
+    if len(detections) < 1:
+        feature.append("error")
+    else:
+        feature.append(landmarks_vectorized)
+
+    for f in feature:
+        if f != "error":
+            feat = np.array(feature, dtype="object")
+            facial_expression = trained_model.predict(feat.reshape(1, -1))[0]
+        else:
+            facial_expression = "anger"
+    # hog = get_hog(image)
+    # feature = hog.compute(image)
+    # feature = reshape_data_extract(feature)
+    # facial_expression = trained_model.predict(feature)[0]
 
     return facial_expression
 
